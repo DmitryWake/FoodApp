773226233be13e0c34204a1c088747ecd808d6be
Dmitry_Wake <nikolaevskiy2001@gmail.com>
2020-10-04 16:52:59 +0300

    Добавление текущей версии в автообновление

--- app/src/main/java/com/example/foodapp/database/databaseValues.kt
+++ app/src/main/java/com/example/foodapp/database/databaseValues.kt
@@ -4,7 +4,7 @@ import com.example.foodapp.models.UserModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 
-const val APP_VERSION = "0.2"
+const val APP_VERSION = "0.3"
 const val CHILD_VERSION = "version"
 
 lateinit var AUTH: FirebaseAuth
